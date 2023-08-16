6cd42c282d41dbb082d8be291a7996057ddf89a4
modestsp <csperichon@gmail.com>
2023-08-16 20:49:45 -0300

    Added styles and github link

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,13 +1,23 @@
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+
 const Footer = () => {
+  const navigate = useNavigate();
+
   return (
-    <footer className="flex mt-auto items-center justify-center gap-3">
+    <footer className="flex mt-auto items-center justify-center gap-3 bg-[#7a62f640] p-3">
       <p>
-        <span>github</span>
+        <span className="flex gap-3 font-medium text-white">
+          <Link to={"https://github.com/modestsp"} target={"_blank"}>
+            <img
+              src="/github-white.png"
+              width={25}
+              alt="visit my github profile"
+            />{" "}
+          </Link>
+          Sebastián Perichón © 2023
+        </span>
       </p>
-      <p>
-        <span>@2023</span>
-      </p>
-      <span>modestsp</span>
     </footer>
   );
 };
