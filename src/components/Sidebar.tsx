08c45a3323ee0ff99ddbe58e7697b9e1b6a5a388
modestsp <csperichon@gmail.com>
2023-08-20 22:16:29 -0300

    Created Sidebar for Account page

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,10 @@
+const Sidebar = () => {
+  return (
+    <div className="flex flex-col text-white border p-4">
+      <p>Account</p>
+      <p>Urls</p>
+    </div>
+  );
+};
+
+export default Sidebar;
