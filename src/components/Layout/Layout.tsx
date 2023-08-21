54db28f992360d198eb775c6663e708c807141c6
modestsp <csperichon@gmail.com>
2023-08-20 22:16:38 -0300

    Updated styles

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -9,7 +9,7 @@ const Layout = ({ children }: ILayout): JSX.Element => {
   return (
     <div className="min-h-screen bg-gradient-to-b from-gray-900 via-[#300171d9] to-slate-900 flex flex-col">
       <Header />
-      <main className="flex-1 h-auto">{children}</main>
+      <main className="flex justify-center flex-1">{children}</main>
       <Footer />
     </div>
   );
