4e5d15fe12aa5480cf1c1c3c8195657cd3690c42
modestsp <csperichon@gmail.com>
2023-08-26 18:28:02 -0300

    Removed unused code

--- src/hooks/useRegister.ts
+++ src/hooks/useRegister.ts
@@ -1,6 +1,6 @@
 import { useMutation, useQueryClient} from "react-query"
 import {  register } from "../services/auth"
-import { UserFromJwt, UserLoginRequest, UserLoginResponse, UserRegisterRequest } from "../types"
+import { UserFromJwt , UserLoginResponse, UserRegisterRequest } from "../types"
 import jwt_decode from "jwt-decode";
 import {  useNavigate } from "react-router-dom";
 
